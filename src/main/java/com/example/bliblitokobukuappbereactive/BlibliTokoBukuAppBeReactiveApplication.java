0eaa0b8322d80ffe4ddfd035e86a44fcd1cab448
Brayan-Esten <brayan.esten@gdn-commerce.com>
2023-03-21 17:39:57 +0700

    initial commit

---
+++ src/main/java/com/example/bliblitokobukuappbereactive/BlibliTokoBukuAppBeReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.bliblitokobukuappbereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlibliTokoBukuAppBeReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlibliTokoBukuAppBeReactiveApplication.class, args);
+	}
+
+}
