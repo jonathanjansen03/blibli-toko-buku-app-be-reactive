e357588b95ad1453aa1d46136245252f48bc997a
WinstarWyvern <winston.kusumo@gdn-commerce.com>
2023-04-14 23:01:11 +0700

    - Add Cache config in book controller

--- src/main/java/com/example/bliblitokobukuappbereactive/controller/BookController.java
+++ src/main/java/com/example/bliblitokobukuappbereactive/controller/BookController.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ExecutionException;
 
 import javax.validation.Valid;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +32,7 @@ public class BookController {
 
   @ApiOperation("Get Books With/out filter")
   @GetMapping()
+  @Cacheable(value = "find-all-books", key = "#title + '-' + #page + '-' + #size")
   public Mono<GetBookWebResponse> getBooks(@RequestParam(required = false) String title,
       @RequestParam(required = false, defaultValue = "1") int page,
       @RequestParam(required = false, defaultValue = "25") int size) {
@@ -43,6 +48,7 @@ public class BookController {
   @ApiOperation("Insert Book")
   @PostMapping(path = "/insert", consumes = {MediaType.APPLICATION_JSON_VALUE},
       produces = {MediaType.APPLICATION_JSON_VALUE})
+  @CacheEvict(value = "find-all-books", allEntries = true)
   public Mono<Book> insertBook(@Valid @RequestBody BookDTO bookDTO) {
     log.info("#insertBook with book request: {}", bookDTO);
     try {
@@ -56,6 +62,13 @@ public class BookController {
   @ApiOperation("Update Book")
   @PutMapping(path = "/update/{bookId}", consumes = {MediaType.APPLICATION_JSON_VALUE},
       produces = {MediaType.APPLICATION_JSON_VALUE})
+  @Caching(
+          evict = {
+          @CacheEvict(value = "find-all-books", allEntries = true)
+  },
+          put = {
+          @CachePut(value = "find-by-id", key = "#id")
+  })
   public Mono<Book> updateBook(@Valid @RequestBody BookDTO bookDTO,
       @PathVariable("bookId") String id) {
     log.info("#updateBook with book request: {}", bookDTO);
@@ -69,6 +82,10 @@ public class BookController {
 
   @ApiOperation("Delete Book")
   @DeleteMapping(path = "/delete/{bookId}")
+  @Caching(evict = {
+          @CacheEvict(value = "find-all-books", allEntries = true),
+          @CacheEvict(value = "find-by-id", key = "#id")
+  })
   public Mono<Void> deleteBook(@PathVariable("bookId") String id) {
     log.info("#deleteBook with book id: {}", id);
     try {
@@ -81,6 +98,7 @@ public class BookController {
 
   @ApiOperation("Find Single Book By Id")
   @GetMapping(path = "/{bookId}")
+  @Cacheable(value = "find-by-id", key = "#id")
   public Mono<Book> findBookById(@PathVariable("bookId") String id) {
     log.info("#findSingleBook with book id: {}", id);
     try {
