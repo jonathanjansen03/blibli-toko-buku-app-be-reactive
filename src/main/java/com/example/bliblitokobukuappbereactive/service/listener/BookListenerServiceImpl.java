c32f2b8fabb621716bc9c64f2bcbf2aa66c96eb7
WinstarWyvern <winston.kusumo@gdn-commerce.com>
2023-04-14 20:32:03 +0700

    - Implement Listener and Publisher Service

--- src/main/java/com/example/bliblitokobukuappbereactive/service/listener/BookListenerServiceImpl.java
+++ src/main/java/com/example/bliblitokobukuappbereactive/service/listener/BookListenerServiceImpl.java
@@ -1,4 +1,32 @@
 package com.example.bliblitokobukuappbereactive.service.listener;
 
-public class BookListenerServiceImpl {
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.example.bliblitokobukuappbereactive.dto.BookDTO;
+import com.example.bliblitokobukuappbereactive.service.BookListenerService;
+import com.example.bliblitokobukuappbereactive.service.BookService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class BookListenerServiceImpl implements BookListenerService {
+  public final static String BOOK_TOPIC = "book_topic";
+
+  public final static String GROUP_ID = "book_group";
+
+  private final BookService bookService;
+
+  public BookListenerServiceImpl(BookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @Override
+  @KafkaListener(topics = BOOK_TOPIC, groupId = GROUP_ID,
+      containerFactory = "KafkaListenerContainerFactory")
+  public void onEventConsumed(BookDTO book) {
+    log.info("Consume Book: {}", book);
+    bookService.insertBook(book).subscribe();
+  }
 }
--- src/main/java/com/example/bliblitokobukuappbereactive/service/publisher/BookPublisherServiceImpl.java
+++ src/main/java/com/example/bliblitokobukuappbereactive/service/publisher/BookPublisherServiceImpl.java
@@ -12,16 +12,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BookPublisherServiceImpl implements BookPublisherService {
 
-	public final static String BOOK_TOPIC = "book_topic";
+  public final static String BOOK_TOPIC = "book_topic";
 
-	private final KafkaTemplate<String, BookDTO> kafkaTemplate;
+  private final KafkaTemplate<String, BookDTO> kafkaTemplate;
 
-	public BookPublisherServiceImpl(KafkaTemplate<String, BookDTO> kafkaTemplate) {
-		this.kafkaTemplate = kafkaTemplate;
-	}
+  public BookPublisherServiceImpl(KafkaTemplate<String, BookDTO> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
 
-	@Override
-	public void sendMessage(BookDTO book) {
-
-	}
+  @Override
+  public void sendMessage(BookDTO book) {
+    log.info("Publish Book: {}", book);
+    this.kafkaTemplate.send(BOOK_TOPIC, book);
+  }
 }
